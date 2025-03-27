b800923dccc78a0716a5d3ef4576319bdd469ba9
NijeshB <nijesh.cse92@gmail.com>
2025-03-27 15:10:07 +0530

    Removing Commented codes

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -1,24 +1,4 @@
 import { PrismaClient } from '@prisma/client';
 const prismaClient = new PrismaClient();
 
-// async function main() {
-//   const user = await prisma.user.create({
-//     data: {
-//       name: "Nijesh",
-//       email: "nijesh.cse92@gmail.com",
-//       password: "Nijesh@134",
-//       mobile: "9944388505",
-//     },
-//   });
-//   console.log(user);
-// }
-
-// main()
-//   .catch((e) => {
-//     logger.error(e.message);
-//   })
-//   .finally(async () => {
-//     await prisma.$disconnect();
-//   });
-
 export default prismaClient;
