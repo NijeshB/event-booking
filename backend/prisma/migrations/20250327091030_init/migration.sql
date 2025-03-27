b9b5c4c805dbaf6c4336fa0f9805f258aa3e5d58
MadhanKumar <madhank34@gmail.com>
2025-03-27 14:44:59 +0530

    Change DB Provide to Postgressql

---
+++ backend/prisma/migrations/20250327091030_init/migration.sql
@@ -0,0 +1,24 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT,
+    "mobile" VARCHAR(15) NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_mobile_key" ON "User"("mobile");
+
+-- CreateIndex
+CREATE INDEX "User_email_idx" ON "User"("email");
