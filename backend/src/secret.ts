48fe61b1de4853302d16631230a08dff4e1215a6
NijeshB <nijesh.cse92@gmail.com>
2025-04-10 14:42:09 +0530

    Removing Logs

--- backend/src/secret.ts
+++ backend/src/secret.ts
@@ -4,3 +4,5 @@ dotenv.config({ path: __dirname + '/../.env' });
 export const secrets = {
   JWT_SECRET: process.env.JWT_SECRET as string,
 };
+
+export const TOKEN_EXPIRES_IN = Number(process.env.TOKEN_EXPIRES_IN ?? 15);
